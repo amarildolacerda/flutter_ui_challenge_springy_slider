e2adfaff7adc66160f5fe97337af5099371f0022
Matt Carroll <me@mattcarroll.name>
2018-05-18 01:23:11 -0700

    Create light and dark slider marks and setup for clipping.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -9,6 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Springy Slider',
       theme: new ThemeData(
         primaryColor: const Color(0xFFFF6688),
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: new MyHomePage(),
     );
@@ -60,7 +63,13 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: new Column(
           children: <Widget>[
-            new Expanded(child: new Container()),
+            new Expanded(
+              child: new SpringySlider(
+                markCount: 12,
+                positiveColor: Theme.of(context).primaryColor,
+                negativeColor: Theme.of(context).scaffoldBackgroundColor,
+              ),
+            ),
             new Container(
               color: Theme.of(context).primaryColor,
               child: new Row(
@@ -75,5 +84,162 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+class SpringySlider extends StatefulWidget {
+  final int markCount;
+  final Color positiveColor;
+  final Color negativeColor;
+
+  SpringySlider({
+    this.markCount = 10,
+    this.positiveColor,
+    this.negativeColor,
+  });
+
+  @override
+  _SpringySliderState createState() => new _SpringySliderState();
+}
+
+class _SpringySliderState extends State<SpringySlider> {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new SliderMarks(
+          markCount: widget.markCount,
+          color: widget.positiveColor,
+          paddingTop: 50.0,
+          paddingBottom: 50.0,
+        ),
+        new LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return new ClipPath(
+              clipper: new SliderClipper(),
+              child: new Stack(
+                children: <Widget>[
+                  new Container(
+                    color: widget.positiveColor,
+                  ),
+                  new SliderMarks(
+                    markCount: widget.markCount,
+                    color: widget.negativeColor,
+                    paddingTop: 50.0,
+                    paddingBottom: 50.0,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class SliderMarks extends StatelessWidget {
+  final int markCount;
+  final Color color;
+  final double paddingTop;
+  final double paddingBottom;
+
+  SliderMarks({
+    this.markCount,
+    this.color,
+    this.paddingTop,
+    this.paddingBottom,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      painter: new SliderMarksPainter(
+        markCount: markCount,
+        color: color,
+        markThickness: 2.0,
+        paddingRight: 20.0,
+        paddingTop: paddingTop,
+        paddingBottom: paddingBottom,
+      ),
+      child: new Container(),
+    );
+  }
+}
+
+class SliderMarksPainter extends CustomPainter {
+  final double largeMarkWidth = 30.0;
+  final double smallMarkWidth = 10.0;
+
+  final int markCount;
+  final Color color;
+  final double markThickness;
+  final double paddingTop;
+  final double paddingBottom;
+  final double paddingRight;
+  final Paint markPaint;
+
+  SliderMarksPainter({
+    this.markCount,
+    this.color,
+    this.markThickness,
+    this.paddingTop = 0.0,
+    this.paddingBottom = 0.0,
+    this.paddingRight = 0.0,
+  }) : markPaint = new Paint()
+          ..color = color
+          ..strokeWidth = markThickness
+          ..style = PaintingStyle.stroke
+          ..strokeCap = StrokeCap.round;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paintHeight = size.height - paddingTop - paddingBottom;
+    final gap = paintHeight / (markCount - 1);
+
+    for (int i = 0; i < markCount; ++i) {
+      double markWidth = smallMarkWidth;
+      if (i == 0 || i == markCount - 1) {
+        markWidth = largeMarkWidth;
+      } else if (i == 1 || i == markCount - 2) {
+        markWidth = lerpDouble(smallMarkWidth, largeMarkWidth, 0.5);
+      }
+
+      double markY = i * gap + paddingTop;
+
+      canvas.drawLine(
+        new Offset(size.width - paddingRight - markWidth, markY),
+        new Offset(size.width - paddingRight, markY),
+        markPaint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class SliderClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path compositePath = new Path();
+
+    compositePath.addRect(
+      new Rect.fromLTWH(
+        0.0,
+        size.height / 2,
+        size.width,
+        size.height / 2,
+      ),
+    );
+
+    return compositePath;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
