141876917261ef63f325e84e3c6c2f8e90bcec72
Matt Carroll <me@mattcarroll.name>
2018-05-18 00:38:31 -0700

    Created app bar and bottom bar. Center of screen is ready for slider.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Springy Slider',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFFFF6688),
       ),
       home: new MyHomePage(),
     );
@@ -21,11 +21,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildTextButton(String title, bool isOnLight) {
+    return new FlatButton(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+      child: new Text(
+        title,
+        style: new TextStyle(
+          fontSize: 12.0,
+          fontWeight: FontWeight.bold,
+          color: isOnLight ? Theme.of(context).primaryColor : Colors.white,
+        ),
+      ),
+      onPressed: () {},
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Container(),
+    return new ClipRRect(
+      borderRadius: new BorderRadius.circular(15.0),
+      child: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          iconTheme: new IconThemeData(
+            color: Theme.of(context).primaryColor,
+          ),
+          elevation: 0.0,
+          leading: new IconButton(
+            icon: new Icon(
+              Icons.menu,
+            ),
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            _buildTextButton('SETTINGS', true),
+          ],
+        ),
+        body: new Column(
+          children: <Widget>[
+            new Expanded(child: new Container()),
+            new Container(
+              color: Theme.of(context).primaryColor,
+              child: new Row(
+                children: <Widget>[
+                  _buildTextButton('MORE', false),
+                  new Expanded(child: new Container()),
+                  _buildTextButton('STATS', false),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
